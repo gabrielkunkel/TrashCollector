fc547dd3f18f53a2d37e04eab0d0fdf09c18c646
gabrielkunkel <henrysaltandson@gmail.com>
2019-11-01 08:07:28 -0500

    easily show customer how much they will owe for this month so they can budget for it

--- TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Web.Mvc;
 using TrashCollector.Models;
@@ -107,6 +108,51 @@ namespace TrashCollector.Controllers
             {
                 return View();
             }
+        }
+
+        public ActionResult OwesThisMonth(string Id)
+        {
+            var customer = dbContext.Customers.Find(Guid.Parse(Id));
+
+            DateTime firstPickUpDayForThisMonthSubtractOne = dbContext.PickUps
+                .Where(pick => pick.CustomerId == customer.CustomerId && pick.Recurring == true && pick.Scheduled.Month == DateTime.Now.Month)
+                .OrderBy(pick => pick.Scheduled)
+                .Select(pick => pick.Scheduled)
+                .FirstOrDefault()
+                .AddDays(-1);
+            DateTime lastDayOfMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month));
+
+            int daysToChargeFor = CountDays(customer.PickUpDay, firstPickUpDayForThisMonthSubtractOne, lastDayOfMonth);
+
+            decimal amountForRecurring = daysToChargeFor * customer.BaseCost;
+
+            decimal totalForAdditional = dbContext.PickUps
+                .Where(pick => pick.CustomerId == customer.CustomerId && pick.Recurring == false && pick.Scheduled.Month == DateTime.Now.Month)
+                .Sum(pick => pick.Cost);
+
+            decimal totalCost = totalForAdditional + amountForRecurring;
+
+            CustomerOwesForThisMonthViewModel model = new CustomerOwesForThisMonthViewModel
+            {
+                CustomerId = customer.CustomerId,
+                OwesThisMonth = totalCost
+            };
+
+            return View(model);
+        }
+
+        private int CountDays(DayOfWeek day, DateTime start, DateTime end)
+        {
+            TimeSpan ts = end - start;                       // Total duration
+            int count = (int)Math.Floor(ts.TotalDays / 7);   // Number of whole weeks
+            int remainder = (int)(ts.TotalDays % 7);         // Number of remaining days
+            int sinceLastDay = (int)(end.DayOfWeek - day);   // Number of days since last [day]
+            if (sinceLastDay < 0) sinceLastDay += 7;         // Adjust for negative days since last [day]
+
+            // If the days in excess of an even week are greater than or equal to the number days since the last [day], then count this one, too.
+            if (remainder >= sinceLastDay) count++;
+
+            return count;
         }
 
         // GET: Customer/Delete/5
--- TrashCollector/Models/CustomerViewModels.cs
+++ TrashCollector/Models/CustomerViewModels.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace TrashCollector.Models
 {
-    public class CustomerDetailsViewModel
+    public class CustomerOwesForThisMonthViewModel
     {
+        public Guid CustomerId { get; set; }
+
+        [Display(Name = "You'll owe for this month")]
+        public decimal OwesThisMonth { get; set; }
 
     }
 }
